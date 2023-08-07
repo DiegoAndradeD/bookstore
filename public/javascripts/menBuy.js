5c58eb6cbe761cc51dab0c654656de59e16ab931
Henriquehmv8 <Henriquehmv8@gmail.com>
2023-08-07 17:04:29 -0300

    barbuy1.0
    1.0

---
+++ public/javascripts/menBuy.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("menuOculto").style.width= "300px";
+    document.getElementById("buyNowBtn").style.marginLeft = "300px";
+}
+
+function closeNav() {
+    document.getElementById("menuOculto").style.width= "0";
+    document.getElementById("buyNowBtn").style.marginLeft = "0";
+}
